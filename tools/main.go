bcb0d92fd011601aad541b6bdb8911a3d9334760
唐健 <tangjian@tangjiandeiMac.local>
2020-11-25 19:10:46 +0800

    提交

--- tools/main.go
+++ tools/main.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"sync"
 	"time"
-
-	"./tcp"
 )
 
 var (
@@ -34,7 +32,68 @@ func testDelete(t map[uint32]string){
 	delete(t,1)
 	fmt.Println("d",t)
 }
+
+const NUM_PRIMES uint32 = 50000
+
+func sieve(v *[NUM_PRIMES]uint32, n *uint32, s *uint32) {
+	var i uint32 = 0
+	for i < *s {
+		if *n % v[i] == 0 {
+			*n++
+			sieve(v, n ,s)
+			return
+		}
+		i++
+	}
+	v[*s] = *n
+	*s++
+}
+
+func calc_n(n int){
+	print("N=",n)
+	var ans int= 0
+	var r1,r2,r3,r4,r5,r6 int
+	var start = time.Now()
+	for a1:= range make([]int,(n>>3)+1){
+		r1=n-a1
+		for a2:=range make([]int,(r1/7)+1){
+			r2=r1-a2
+			for a3:=range make([]int,(r2/6)+1){
+				r3=r2-a3
+				for a4:=range make([]int,(r3/5)+1){
+					r4=r3-a4
+					for a5:=range make([]int,(r4>>2)+1){
+						r5=r4-a5
+						for a6:=range make([]int,(r5/3)+1){
+							r6=r5-a6
+							for a7:=range make([]int,(r6>>1)+1){
+								ans+=a1^a2^a3^a4^a5^a6^a7^(r6-a7)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	var end = time.Now()
+	println(end.Sub(start).Milliseconds())
+}
+
+
 func main() {
+	calc_n(600)
+	//var size uint32 = 0
+	//var i uint32 = 2
+	//var primes [NUM_PRIMES]uint32
+	//var start = time.Now()
+	//for size < NUM_PRIMES {
+	//	sieve(&primes, &i, &size)
+	//}
+	////for k := NUM_PRIMES-10; k < NUM_PRIMES; k++ {
+	////	fmt.Printf("%d\n", primes[k])
+	////}
+	//var end = time.Now()
+	//println(end.Sub(start).Milliseconds())
 	//var c = make(chan int)
 	//go testSend(c)
 	//testRec(c)
@@ -46,9 +105,9 @@ func main() {
 	//for k,v:= range test{
 	//	println(k,v)
 	//}
-	sh:=tcp.ServerHandler{0,tcp.TcpSession{nil,0}}
-	go tcp.InitTcpServer("127.0.0.1:8080",sh)
-	tcp.InitTcpClient("127.0.0.1:8080")
+	//sh:=tcp.ServerHandler{0,tcp.TcpSession{nil,0}}
+	//go tcp.InitTcpServer("127.0.0.1:8080",sh)
+	//tcp.InitTcpClient("127.0.0.1:8080")
 	//a := time.Now().Nanosecond()
 	//i := 1
 	//for i < 10000000 {
